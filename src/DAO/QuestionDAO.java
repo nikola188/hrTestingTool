0d4b4eaa3b0179f7fc820c03a741cbee2c455a94
Dusan Cvijic <dusan.cvijic@eng.it>
2018-07-25 15:39:09 +0200

    Question randomizer completed and testable

--- src/DAO/QuestionDAO.java
+++ src/DAO/QuestionDAO.java
@@ -7,8 +7,10 @@ package DAO;
 
 import Beans.Question;
 import java.util.List;
+import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  *
@@ -60,4 +62,39 @@ public class QuestionDAO {
         s.close();
         return true;
     } 
+    
+    public static List<Question> getQuestionsByCandidateId(int candidateId){
+    
+        List<Question> questionList = null;
+        Session s = Hibernate.HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+
+        try {
+
+            tx = s.beginTransaction();
+
+            String queryString = "Select q FROM Question as q JOIN q.idTechnology as t WHERE t.id IN (SELECT ct.idTechnology FROM CandidateTechnology ct WHERE ct.idCandidate = :idCandidateParam)";
+            
+            
+            Query query = s.createQuery(queryString);
+            query.setInteger("idCandidateParam", candidateId);
+            questionList = query.list();
+
+            tx.commit();
+
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            if (tx != null) {
+                tx.rollback();
+            }
+        } finally {
+            s.close();
+        }
+        if(questionList == null || questionList.isEmpty()){
+            return null;
+        }
+
+        return questionList;
+        
+    }
 }
---
+++ src/test/Test.java
@@ -0,0 +1,70 @@
+package test;
+
+import Core.GenerateQuestion;
+import Hibernate.HibernateUtil;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import util2.TestQuestion;
+
+public class Test {
+    
+   private static SessionFactory factory;
+   private static Session session;
+   public static void main(String[] args) {
+       try{
+            System.out.println("Test start");
+            factory = HibernateUtil.getSessionFactory();
+            try {
+                session = factory.openSession();
+                System.out.println("Session is open");
+            } catch (Throwable ex) {
+                System.err.println("Failed to create sessionFactory object." + ex);
+                throw new ExceptionInInitializerError(ex);
+            }
+
+
+            System.out.println("Testing database connection...");
+            Query query = session.createQuery("from Technology");
+            List<Beans.Technology> technologies = query.list();
+            System.out.println(technologies.size());
+            query = session.createQuery("from Question");
+            List<Beans.Question> question = query.list();
+            System.out.println(question.size());
+            query = session.createQuery("from Candidate");
+            List<Beans.Candidate> candidate = query.list();
+            System.out.println(candidate.size());
+            System.out.println("Most important tables tested!");
+
+            System.out.println("Testing GenerateQuestion for candidateId 2 who knows 4 technologies");
+
+            int candidateId = 1;
+            int numberOfQuestions = 1;
+            
+            GenerateQuestion.getQuestions(candidateId, numberOfQuestions);
+            List<TestQuestion> questions = GenerateQuestion.getList();
+            
+            System.out.println("list length:   " + questions.size());
+
+            if(questions != null && !questions.isEmpty()){
+                for(int i = 0; i < questions.size(); i++){
+                    TestQuestion q = questions.get(i);
+                    System.out.print(q.getQuestion().getId() + " ");
+                    System.out.print(q.getQuestion().getIdTechnology() + " ");
+                    System.out.print(q.getQuestion().getPoints() + " ");
+                    System.out.print(q.getQuestion().getText() + " ");
+                    System.out.println(q.getList().get(0).getText());
+                    System.out.println("");
+                }
+            } else {
+                System.out.println("No questions found.");
+            }
+            System.out.println("Test completed!");
+
+        }catch(Exception ex){
+           ex.printStackTrace();
+        }
+   }
+    
+}
