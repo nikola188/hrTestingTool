87f9e75d69b04d97d330e5974e833ae2c12d3ebc
IgorN95 <igor.neskovic@eng.it>
2018-07-25 15:05:15 +0200

    Inserted data for candidate, candidate_technology, technology, and question in database. @Author Igor Neskovic

--- first (1).sql
+++ first (1).sql
@@ -1,217 +1,188 @@
--- phpMyAdmin SQL Dump
--- version 4.8.2
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 25, 2018 at 11:39 AM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.2.7
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: first
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.34-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `first`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `answers`
 --
 
+DROP TABLE IF EXISTS `answers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `answers` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `text` varchar(256) NOT NULL,
   `id_question` int(11) NOT NULL,
-  `is_correct` tinyint(1) NOT NULL
+  `is_correct` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_question` (`id_question`),
+  CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`id_question`) REFERENCES `question` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `answers`
+--
+
+LOCK TABLES `answers` WRITE;
+/*!40000 ALTER TABLE `answers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `answers` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `candidate`
 --
 
+DROP TABLE IF EXISTS `candidate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `candidate` (
-  `id` int(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `candidate`
+--
+
+LOCK TABLES `candidate` WRITE;
+/*!40000 ALTER TABLE `candidate` DISABLE KEYS */;
+INSERT INTO `candidate` VALUES (1),(2),(3),(4),(5);
+/*!40000 ALTER TABLE `candidate` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `candidate_technology`
 --
 
+DROP TABLE IF EXISTS `candidate_technology`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `candidate_technology` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_candidate` int(11) DEFAULT NULL,
-  `id_technology` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_technology` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_candidate` (`id_candidate`),
+  KEY `id_technology` (`id_technology`),
+  CONSTRAINT `candidate_technology_ibfk_1` FOREIGN KEY (`id_candidate`) REFERENCES `candidate` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `candidate_technology_ibfk_2` FOREIGN KEY (`id_technology`) REFERENCES `technology` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `candidate_technology`
+--
+
+LOCK TABLES `candidate_technology` WRITE;
+/*!40000 ALTER TABLE `candidate_technology` DISABLE KEYS */;
+INSERT INTO `candidate_technology` VALUES (2,1,3),(3,2,5),(4,1,6),(5,2,3),(6,3,7),(7,3,8),(8,4,9),(9,4,10),(10,5,10),(11,5,11),(12,4,11),(13,5,3),(14,3,2),(15,2,10),(16,2,6),(17,4,3);
+/*!40000 ALTER TABLE `candidate_technology` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `question`
 --
 
+DROP TABLE IF EXISTS `question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `question` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `text` varchar(128) NOT NULL,
   `id_technology` int(11) NOT NULL,
   `multi_answer` tinyint(1) DEFAULT NULL,
-  `points` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `points` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_technology` (`id_technology`),
+  CONSTRAINT `question_ibfk_1` FOREIGN KEY (`id_technology`) REFERENCES `technology` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `question`
+--
+
+LOCK TABLES `question` WRITE;
+/*!40000 ALTER TABLE `question` DISABLE KEYS */;
+INSERT INTO `question` VALUES (1,'first question for java',1,1,4),(2,'secound question for java',1,1,4),(3,'third question java',1,0,3),(4,'fourth question java',1,0,3),(5,'fifth question java',1,0,3),(6,'first question angular',6,1,4),(7,'Secound question angular',6,1,4),(8,'third question Angular',6,0,3),(9,'fourth question Angular',6,0,3),(10,'fifth question Angular',6,0,3),(11,'first question php',3,1,4),(12,'secound question php',3,1,4),(13,'third question php',3,0,3),(14,'fourth question php',3,0,3),(15,'fifth question php',3,0,3),(16,'First question C++',8,1,4),(17,'Secound question C++',8,1,4),(18,'third question C++',8,0,3),(19,'Fourth question C++',8,0,3),(20,'Fifth question C++',8,0,3);
+/*!40000 ALTER TABLE `question` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `result`
 --
 
+DROP TABLE IF EXISTS `result`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `result` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_candidate` int(11) NOT NULL,
   `id_technology` int(11) NOT NULL,
-  `result` int(11) NOT NULL
+  `result` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_candidate` (`id_candidate`),
+  KEY `id_technology` (`id_technology`),
+  CONSTRAINT `result_ibfk_1` FOREIGN KEY (`id_candidate`) REFERENCES `candidate` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `result_ibfk_2` FOREIGN KEY (`id_technology`) REFERENCES `technology` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `result`
+--
+
+LOCK TABLES `result` WRITE;
+/*!40000 ALTER TABLE `result` DISABLE KEYS */;
+/*!40000 ALTER TABLE `result` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `technology`
 --
 
+DROP TABLE IF EXISTS `technology`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `technology` (
-  `id` int(11) NOT NULL,
-  `text` varchar(128) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `technology`
 --
 
---
--- Indexes for table `answers`
---
-ALTER TABLE `answers`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_question` (`id_question`);
+LOCK TABLES `technology` WRITE;
+/*!40000 ALTER TABLE `technology` DISABLE KEYS */;
+INSERT INTO `technology` VALUES (1,'JAVA'),(2,'SQL'),(3,'PHP'),(4,'PYTHON'),(5,'JAVASCRIPT'),(6,'ANGULAR'),(7,'REACT'),(8,'C++'),(9,'C#'),(10,'SPRING'),(11,'MONGO');
+/*!40000 ALTER TABLE `technology` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for table `candidate`
---
-ALTER TABLE `candidate`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `candidate_technology`
---
-ALTER TABLE `candidate_technology`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_candidate` (`id_candidate`),
-  ADD KEY `id_technology` (`id_technology`);
-
---
--- Indexes for table `question`
---
-ALTER TABLE `question`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_technology` (`id_technology`);
-
---
--- Indexes for table `result`
---
-ALTER TABLE `result`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_candidate` (`id_candidate`),
-  ADD KEY `id_technology` (`id_technology`);
-
---
--- Indexes for table `technology`
---
-ALTER TABLE `technology`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `answers`
---
-ALTER TABLE `answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `candidate`
---
-ALTER TABLE `candidate`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `candidate_technology`
---
-ALTER TABLE `candidate_technology`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `question`
---
-ALTER TABLE `question`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `result`
---
-ALTER TABLE `result`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `technology`
---
-ALTER TABLE `technology`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `answers`
---
-ALTER TABLE `answers`
-  ADD CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`id_question`) REFERENCES `question` (`id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `candidate_technology`
---
-ALTER TABLE `candidate_technology`
-  ADD CONSTRAINT `candidate_technology_ibfk_1` FOREIGN KEY (`id_candidate`) REFERENCES `candidate` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `candidate_technology_ibfk_2` FOREIGN KEY (`id_technology`) REFERENCES `technology` (`id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `question`
---
-ALTER TABLE `question`
-  ADD CONSTRAINT `question_ibfk_1` FOREIGN KEY (`id_technology`) REFERENCES `technology` (`id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `result`
---
-ALTER TABLE `result`
-  ADD CONSTRAINT `result_ibfk_1` FOREIGN KEY (`id_candidate`) REFERENCES `candidate` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `result_ibfk_2` FOREIGN KEY (`id_technology`) REFERENCES `technology` (`id`) ON UPDATE CASCADE;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-07-25 14:55:44
